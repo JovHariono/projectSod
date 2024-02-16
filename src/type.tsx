1721b38df690e590296220df884c0e8da3f46136
JovHariono <128022304+JovHariono@users.noreply.github.com>
2024-02-16 22:59:03 +0700

    projectSod

---
+++ src/type.tsx
@@ -0,0 +1,13 @@
+import { StaticImageData } from "next/image";
+
+export interface BrandType {
+  image: StaticImageData;
+  judul: string;
+  imagePopup: StaticImageData[];
+  judulPopUp: string;
+  deskripsiPopUp: string;
+  instagram: string;
+  twitter: string;
+  tiktok: string;
+  youtube: string;
+}
